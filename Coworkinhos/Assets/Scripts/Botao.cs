060b4964b54a43a3ebafc52be8660b2698d9c4f7
hudo9921 <57452786+hudo9921@users.noreply.github.com>
2022-08-31 20:51:32 -0300

    Plataforma+Botao

---
+++ Coworkinhos/Assets/Scripts/Botao.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botao : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool pisou;
+
+    public GameObject plataforma;
+
+    public float velocidade;
+    public Transform posicaoAtual;
+    public Transform[] pontos;
+
+    public int pontoSelecao;
+
+    void Start()
+    {
+        posicaoAtual = pontos[pontoSelecao];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pisou==true)
+        {
+            Mover();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D colisao)
+    {
+
+        if(colisao.gameObject.tag == "Player")
+        {
+            pisou =true;
+            //Debug.Log("pisou botao");
+        }
+    }
+    void OnCollisionExit2D(Collision2D colisao)
+    {
+
+        if(colisao.gameObject.tag == "Player")
+        {
+            pisou =false;
+            //Debug.Log("pisou botao");
+        }
+    }
+
+    void Mover()
+    {
+        //Debug.Log("movendo");
+        plataforma.transform.position = Vector2.MoveTowards(plataforma.transform.position,posicaoAtual.position,Time.deltaTime*velocidade);
+
+       if(plataforma.transform.position.y == pontos[pontoSelecao].transform.position.y)
+       {
+        pontoSelecao++;
+        if(pontoSelecao==pontos.Length)
+        {
+            pontoSelecao=0;
+        }
+        posicaoAtual=pontos[pontoSelecao];
+       }
+    }
+
+}
